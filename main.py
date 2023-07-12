ee5a56054dcef6e28f09d8dad449846fdad8bdbe
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-12 10:42:59 +0200

    Inserting WC2022GroupC in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy.orm import Session
 from models import Base, Team, Match
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
@@ -13,3 +14,26 @@ if __name__ == '__main__':
 
     Base.metadata.create_all(engine)
 
+    with Session(engine) as session:
+        argentina = Team(name="Argentina")
+        poland = Team(name="Poland")
+        saudi_arabia = Team(name="Saudi Arabia")
+        mexico = Team(name="Mexico")
+        session.add_all([argentina, poland, saudi_arabia, mexico])
+        session.commit()
+        match_0 = Match(first_team_id=argentina.id, second_team_id=saudi_arabia.id,
+                        first_team_score=1, second_team_score=2)
+        match_1 = Match(first_team_id=mexico.id, second_team_id=poland.id,
+                        first_team_score=0, second_team_score=0)
+        match_2 = Match(first_team_id=poland.id, second_team_id=saudi_arabia.id,
+                        first_team_score=2, second_team_score=0)
+        match_3 = Match(first_team_id=argentina.id, second_team_id=mexico.id,
+                        first_team_score=2, second_team_score=0)
+        match_4 = Match(first_team_id=poland.id, second_team_id=argentina.id,
+                        first_team_score=0, second_team_score=2)
+        match_5 = Match(first_team_id=saudi_arabia.id, second_team_id=mexico.id,
+                        first_team_score=1, second_team_score=2)
+
+        session.add_all([match_0, match_1, match_2, match_3, match_4, match_5])
+
+        session.commit()

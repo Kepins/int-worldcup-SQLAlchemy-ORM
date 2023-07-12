dd37dab385d78d8ce38cde2b204e08ae48be87f2
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-12 11:53:17 +0200

    Connecting to PostgreSQL

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 from models import Base, Team, Match
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    engine = create_engine("sqlite://", echo=True)
+    load_dotenv()
+    user = os.environ['POSTGRESQL_USER']
+    passwd = os.environ['POSTGRESQL_PASSWD']
+    hostname = os.environ['POSTGRESQL_HOSTNAME']
+    db_name = os.environ['POSTGRESQL_DB_NAME']
+    engine = create_engine(f'postgresql+psycopg2://{user}:{passwd}@{hostname}/{db_name}')
 
     Base.metadata.create_all(engine)
 

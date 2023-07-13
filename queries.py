0b6501f97320b507749b825154d13fd9db7a386f
Szymon Kępiński <szymon.kepinski@profil-software.com>
2023-07-13 08:48:18 +0200

    Query two best teams

--- queries.py
+++ queries.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, func, case, desc
 from sqlalchemy.orm import Session
 
 from models import Team, Match
@@ -9,7 +9,8 @@ def db_empty(session):
     if num_teams == 0:
         return True
 
-def insert_WC2022GroupC(session):
+
+def insert_WC2022GroupC(session: Session):
     argentina = Team(name="Argentina")
     poland = Team(name="Poland")
     saudi_arabia = Team(name="Saudi Arabia")
@@ -33,11 +34,50 @@ def insert_WC2022GroupC(session):
 
     session.commit()
 
-def two_best_teams(session):
+
+def two_best_teams(session: Session):
+
+    scores_as_first_team = select(
+        Team,
+        Match.first_team_score.label("scored"),
+        Match.second_team_score.label("conceded")
+    ).join(Team.in_first_team)
+
+    scores_as_second_team = select(
+        Team,
+        Match.second_team_score.label("scored"),
+        Match.first_team_score.label("conceded")
+    ).join(Team.in_second_team)
+
+    results = scores_as_first_team.union_all(scores_as_second_team)
+
+    standings = \
+        select(
+            results.c.id,
+            results.c.name,
+            func.sum(
+                case(
+                    (results.c.scored > results.c.conceded, 3),
+                    (results.c.scored == results.c.conceded, 1),
+                    else_=0
+                )
+            ).label("points"),
+            func.sum((results.c.scored - results.c.conceded)).label("goal_diff"),
+            func.sum(results.c.scored).label("scored")
+        )\
+        .group_by(results.c.id, results.c.name)\
+        .order_by(desc("points"), desc("goal_diff"), desc("scored"))
+
+    two_teams = select(
+        standings.c.id,
+        standings.c.name
+    ).limit(2)
+
+    return session.execute(two_teams).all()
+
+
+def max_points(session: Session):
     pass
 
-def max_points(session):
-    pass
-
-def worst_team_name(session):
+def worst_team_name(session: Session):
     pass
